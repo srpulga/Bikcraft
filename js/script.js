cc397ab8da1743927b78bb0e190e343d4d3d74ab
Eduardo Galvão <60319097+srpulga@users.noreply.github.com>
2020-08-28 08:51:25 -0300

    add js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if (window.SimpleSlide) {
+
+    new SimpleSlide({
+        slide: "quote",
+        time: 5000,
+    });
+
+    new SimpleSlide({
+        slide: "portifolio",
+        time: 5000,
+        nav: true
+    })
+
+}
